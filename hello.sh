b4b0d57b1eb72504e4d125f17b1184fe9a75fd08
lsetga <llybisetga@gmail.com>
2024-01-25 05:46:47 +0300

    Zone-01-first task

---
+++ hello.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+echo "Hello Negativeice!"
+# curl https:// api.github.com/users (to get info from github).
+# ls 
+# touch (1..100)
+# ls ( prints out 1 to 100, i.e 1 2 3 4 5...)
+# find 10
+# output : 10
+# find 1000
+# output : no such file or directory
+# All files that have one somewhere
+# to delete all 100 empty files, rm {1..100}
+: << 'COMMENT'
+C:\home\osodo\Desktop\Zone-01> find -name '1*'
+./13
+./18
+./1
+./14
+./16
+./12
+./17
+./11
+./15
+./100
+./19
+./10
+COMMENT
