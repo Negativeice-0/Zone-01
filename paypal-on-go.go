7c68de9f53f1a1b6904b554d8d701fc2efebda24
lsetga <llybisetga@gmail.com>
2024-01-29 20:58:45 +0300

    paypal-origins

---
+++ paypal-on-go.go
@@ -0,0 +1,21 @@
+The selected text is from a blog post discussing how PayPal managed to scale to billions of daily transactions using just eight virtual machines.
+ Here are the key points:
+
+- The author was intrigued by a Reddit post about PayPal's impressive scaling and decided to 
+investigate further.
+
+- The blog post shares the findings and also includes some Go code to provide a hands-on 
+understanding of the
+ technology behind this achievement.
+- PayPal, created by a team of engineers in 1998, experienced a surge in popularity and a
+ substantial increase 
+in traffic during its early days.
+- To cope with the demand, they invested in newer hardware to scale. However, the rapid 
+growth led to a 
+milestone of 1 million transactions a day within two years.
+- To keep up with this growth, they expanded their infrastructure to more than 1000 virtual
+ machines.
+ While this addressed their scalability challenges
+, it also introduced new complexities and issues related to managing a larger and more 
+intricate system. 
+The blog post goes on to discuss these challenges.
