f41ff736be1ebf57a572e540deb7c7a015b495ad
lsetga <llybisetga@gmail.com>
2024-01-30 07:00:59 +0300

    some-overall, changes

--- even.go
+++ even.go
@@ -3,16 +3,18 @@ package main
 import "fmt"
 
 func main() {
-var num int
+	var num int
+	var i int
 
-fmt.Print("Enter a number: ")
-fmt.Scanln(&num)
+	fmt.Print("Enter a number: ")
+	fmt.Scanln(&num)
 
-for i = '0'; i > num; i-- {
-if num%2 == 0 {
-fmt.Println("The entered number is even:", num)
-} else {
-fmt.Println("The entered number is not even.")
-}
-}
+	for i = '0'; i > num; i-- {
+		if num%2 == 0 {
+			fmt.Println("The entered number is even:", num)
+			fmt.Print(i)
+		} else {
+			fmt.Println("The entered number is not even.")
+		}
+	}
 }
